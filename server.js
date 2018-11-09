37dfa9fa3575f433985e9b8841020a0067e36b78
Ryan Stachura <stachurr@oregonstate.edu>
2018-11-09 12:49:48 -0800

    improved readability

--- server.js
+++ server.js
@@ -6,59 +6,55 @@
  * email:   stachurr@oregonstate.edu
  */
 
-/*Variables*/
+/*--------------------------------------------- Variable initialization*/
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(requestHandler);
 var page_index_html, page_index_js, page_404_html, page_style_css, image_benny = null;
 
-/*Read files and store*/
+/*--------------------------------------------- Read files and store*/
 fs.readFile("./public/index.html", "utf-8", function(err, data) {
     if (err) { throw err; }
     page_index_html = data;
-    console.log("[index.html successfully read]");
+    console.log("[file successfully read: index.html]");
 });
 
 fs.readFile("./public/index.js", "utf-8", function(err, data) {
     if (err) { throw err; }
     page_index_js = data;
-    console.log("[index.js successfully read]");
+    console.log("[file successfully read: index.js]");
 });
 
 fs.readFile("./public/404.html", "utf-8", function(err, data) {
     if (err) { throw err; }
     page_404_html = data;
-    console.log("[404.html successfully read]");
+    console.log("[file successfully read: 404.html]");
 });
 
 fs.readFile("./public/style.css", "utf-8", function(err, data) {
     if (err) { throw err; }
     page_style_css = data;
-    console.log("[style.css successfully read]");
+    console.log("[file successfully read: style.css]");
 });
 
 fs.readFile("./public/benny.jpg", function(err, data) {
     if (err) { throw err; }
     image_benny = data;
-    console.log("[benny.jpg successfully read]");
+    console.log("[file successfully read: benny.jpg]");
 });
 
-/*Start server*/
+/*--------------------------------------------- Start server*/
 server.listen(3000, function(err) {
     if (err) { throw err; }
     console.log("[Server is listening on port 3000]");
 });
 
-/*Request handler*/
+/*--------------------------------------------- Request handler*/
 function requestHandler(req, res) {
     console.log("[Request received]");
 
     switch(req.url) {
         case "/":
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(page_index_html);
-            break;
-
         case "/index.html":
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(page_index_html);
