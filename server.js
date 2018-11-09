7a99a9a85fdc33a2aa2392c32f52e9587d94aecd
Ryan Stachura <stachurr@oregonstate.edu>
2018-11-08 16:13:59 -0800

    final

--- server.js
+++ server.js
@@ -2,6 +2,87 @@
  * Write your server code in this file.  Don't forget to add your name and
  * @oregonstate.edu email address below.
  *
- * name:
- * email:
+ * name:    Ryan Stachura
+ * email:   stachurr@oregonstate.edu
  */
+
+/*Variables*/
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(requestHandler);
+var page_index_html, page_index_js, page_404_html, page_style_css, image_benny = null;
+
+/*Read files and store*/
+fs.readFile("./public/index.html", "utf-8", function(err, data) {
+    if (err) { throw err; }
+    page_index_html = data;
+    console.log("[index.html successfully read]");
+});
+
+fs.readFile("./public/index.js", "utf-8", function(err, data) {
+    if (err) { throw err; }
+    page_index_js = data;
+    console.log("[index.js successfully read]");
+});
+
+fs.readFile("./public/404.html", "utf-8", function(err, data) {
+    if (err) { throw err; }
+    page_404_html = data;
+    console.log("[404.html successfully read]");
+});
+
+fs.readFile("./public/style.css", "utf-8", function(err, data) {
+    if (err) { throw err; }
+    page_style_css = data;
+    console.log("[style.css successfully read]");
+});
+
+fs.readFile("./public/benny.jpg", function(err, data) {
+    if (err) { throw err; }
+    image_benny = data;
+    console.log("[benny.jpg successfully read]");
+});
+
+/*Start server*/
+server.listen(3000, function(err) {
+    if (err) { throw err; }
+    console.log("[Server is listening on port 3000]");
+});
+
+/*Request handler*/
+function requestHandler(req, res) {
+    console.log("[Request received]");
+
+    switch(req.url) {
+        case "/":
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(page_index_html);
+            break;
+
+        case "/index.html":
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(page_index_html);
+            break;
+
+        case "/style.css":
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(page_style_css);
+            break;
+
+        case "/index.js":
+            res.writeHead(200, {'Content-Type': 'application/javascript'});
+            res.write(page_index_js);
+            break;
+
+        case "/benny.jpg":
+            res.writeHead(200, {'Content-Type': 'image/jpg'});
+            res.write(image_benny);
+            break;
+
+        default:
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(page_404_html);
+    }
+
+    res.end();
+}
